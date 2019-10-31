23136766cbcc6abf221e518d6fc2a7e6dd1c920c
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-31 18:56:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,11 +10,11 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls
 end
 
-def long_planeteer_calls
+def long_planeteer_calls(array)
+array.any? {|call| call.length>4}
   
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheeses
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
