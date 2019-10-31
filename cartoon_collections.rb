8620df5bd961da883fa9d39f892ec836e08bec4d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-31 18:38:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,12 @@
 def roll_call_dwarves(dwarves)
   dwarves=["Doc","Dopey","Bashful","Grumpy"]
-  #
+  #counter = 0
+#array = [1,2,3,4,5]
+ 
+#while array[counter] do
+  #puts array[counter]
+ # counter += 1
+end
 end
 end 
 
